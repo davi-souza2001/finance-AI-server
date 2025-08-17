355616ea18d3d7fc02d50d53a4f9062e0c621a3a
Davi Souza <davisouza2001dv@gmail.com>
2025-08-17 10:59:14 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -29,6 +29,9 @@ app.register(fastifyJwt, {
 
 app.register(fastifyCors, {
   origin: ['http://localhost:5173', 'http://localhost:4173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 })
 
 app.register(fastifyMultipart)
